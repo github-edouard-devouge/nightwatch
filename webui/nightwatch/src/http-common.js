39b1334ed0ac9309d87aa85d8d46ca2091042c52
Edouard Devouge <edevouge@advalo.com>
2020-07-08 16:20:18 +0200

    fix(webui): fix axios baseurl

--- webui/nightwatch/src/http-common.js
+++ webui/nightwatch/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "/api/v1",
   headers: {
     "Content-type": "application/json"
   }
